182fee6cb417d0dbc4e8e51642b4d144ec1b0f1f
Ray <rayalexvector@gmail.com>
2026-07-01 19:21:36 +0800

    Bust arcade cloud save cache

--- arcade/starfall/js/app.js
+++ arcade/starfall/js/app.js
@@ -315,7 +315,7 @@
     if (!('serviceWorker' in navigator)) return;
     const okOrigin = location.protocol === 'https:' || location.hostname === 'localhost' || location.hostname === '127.0.0.1';
     if (!okOrigin) return;
-    navigator.serviceWorker.register('./sw.js').catch(() => {});
+    navigator.serviceWorker.register('./sw.js?v=cloudsave-1').catch(() => {});
   }
 
   function boot(){
